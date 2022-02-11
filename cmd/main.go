f78706bb43fd44a13229d83ab569be339834ec8e
mad programmer <miskadl09@gmail.com>
2022-02-11 13:14:55 +0200

    remove timezone ..

--- cmd/main.go
+++ cmd/main.go
@@ -38,7 +38,7 @@ func main() {
 		},
 	)
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Europe/Kiev", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DBNAME"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DBNAME"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: newLogger, FullSaveAssociations: true})
 
 	internal.SetDB(db)
