290b3487d7d5a68e62fd9baec45736d74fc2a749
mlazebny <miskadl09@gmail.com>
2022-05-02 10:10:17 +0300

    add date_created

--- .helm/migration/V1__CreateSchedule.sql
+++ .helm/migration/V1__CreateSchedule.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS schedules
 (
-    id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    chat_id BIGINT NOT NULL
+    id           UUID PRIMARY KEY         DEFAULT gen_random_uuid(),
+    chat_id      BIGINT NOT NULL,
+    date_created TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS days
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     schedule_id UUID NOT NULL,
-    caption     TEXT DEFAULT '',
+    caption     TEXT             DEFAULT '',
     CONSTRAINT fk_schedule FOREIGN KEY (schedule_id)
         REFERENCES schedules (id)
         ON DELETE CASCADE
@@ -18,7 +19,7 @@ CREATE TABLE IF NOT EXISTS pairs
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     day_id      UUID NOT NULL,
-    title       TEXT DEFAULT '',
+    title       TEXT             DEFAULT '',
     information TEXT,
     CONSTRAINT fk_day FOREIGN KEY (day_id)
         REFERENCES days (id)
